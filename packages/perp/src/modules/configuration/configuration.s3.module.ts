67ec0a1a57ebc187ecd0ad64c8209b0b674e065b
liulinhui <1249055292@qq.com>
2025-08-25 15:12:57 +0800

    fix: s3 support any chain

--- packages/perp/src/modules/configuration/configuration.s3.module.ts
+++ packages/perp/src/modules/configuration/configuration.s3.module.ts
@@ -1,27 +1,31 @@
 import axios from 'axios';
-import { CHAIN_ID, Context } from '@derivation-tech/context';
+import { Context } from '@derivation-tech/context';
 import { SynFuturesConfig } from '../../types';
 import { loadConfig } from '../../utils';
 import { ConfigurationModuleBase } from './configuration.base.module';
-
-// TODO: improve @samlior
-const suppotedChainId = new Set<CHAIN_ID>([CHAIN_ID.BASE, CHAIN_ID.BLAST, CHAIN_ID.MONADTESTNET]);
+import { LocalConfigurationModule } from './configuration.local.module';
 
 export class S3ConfigurationModule extends ConfigurationModuleBase {
     basePath: string;
+    localConfigurationModule: LocalConfigurationModule;
 
     constructor(context: Context, basePath?: string) {
         super(context);
         this.basePath = basePath ?? 'https://api.synfutures.com/s3/config/sdkConfig';
+        this.localConfigurationModule = new LocalConfigurationModule(context);
     }
 
     protected async getConfig(): Promise<SynFuturesConfig | null> {
-        if (!suppotedChainId.has(this.context.chainId)) {
-            return null;
-        }
-
         const url = this.basePath + '/' + this.context.chainId + '.json';
         const res = await axios.get(url);
+        if (res.status !== 200) {
+            return this.localConfigurationModule.config;
+        }
         return loadConfig(res.data);
+    }
+
+    async update(): Promise<void> {
+        await this.localConfigurationModule.update();
+        await super.update();
     }
 }
