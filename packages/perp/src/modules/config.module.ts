ddfad0b8339c6975e04225391f37d25840cd8dac
allan-wei-tx <1370485525@qq.com>
2025-08-26 21:00:05 +0800

    Fix perp config method

--- packages/perp/src/modules/config.module.ts
+++ packages/perp/src/modules/config.module.ts
@@ -6,6 +6,16 @@ import { Config as LegacyConfig } from 'src/typechain';
 export class ConfigModule implements ConfigInterface {
     context: Context;
 
+    LEGACY_NETWORKS = [
+        CHAIN_ID.ARBITRUM,
+        CHAIN_ID.BLAST,
+        CHAIN_ID.BLASTSEPOLIA,
+        CHAIN_ID.GOERLI,
+        CHAIN_ID.LINEA,
+        CHAIN_ID.POLYGON,
+        CHAIN_ID.SCROLL,
+    ];
+
     constructor(context: Context) {
         this.context = context;
     }
@@ -14,56 +24,53 @@ export class ConfigModule implements ConfigInterface {
         let calls = [];
         let results: boolean[] = [];
         let configInterface: ethers.utils.Interface = this.context.perp.contracts.config.interface;
-        if (
-            (this.context.chainId === CHAIN_ID.BASE ||
-                this.context.chainId === CHAIN_ID.MONADTESTNET ||
-                this.context.chainId === CHAIN_ID.LOCAL) &&
-            quoteAddr
-        ) {
+
+        // legacy function for networks that use the legacy ABI version
+        if (this.LEGACY_NETWORKS.includes(this.context.chainId)) {
+            calls = [];
+            results = [];
+            configInterface = new ethers.utils.Interface([
+                'function lpWhitelist(address user) external view returns (bool)',
+            ]);
+
             for (const trader of traders) {
                 calls.push({
                     target: this.context.perp.contracts.config.address,
-                    callData: configInterface.encodeFunctionData('lpWhitelist', [quoteAddr, trader]),
+                    callData: configInterface.encodeFunctionData('lpWhitelist', [trader]),
                 });
             }
-            try {
-                const rawData = await this.context.multiCall3.callStatic.aggregate(calls, overrides ?? {});
-                for (const data of rawData.returnData) {
-                    results.push(configInterface.decodeFunctionResult('lpWhitelist', data)[0]);
-                }
-                return results;
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            } catch (e) {
-                // ignore error since the contract on some network may not have this function
+            const rawData = await this.context.multiCall3.callStatic.aggregate(calls, overrides ?? {});
+            for (const data of rawData.returnData) {
+                results.push(configInterface.decodeFunctionResult('lpWhitelist', data)[0]);
             }
+            return results;
         }
-        // legacy function for other networks
-        calls = [];
-        results = [];
-        configInterface = new ethers.utils.Interface([
-            'function lpWhitelist(address user) external view returns (bool)',
-        ]);
 
         for (const trader of traders) {
             calls.push({
                 target: this.context.perp.contracts.config.address,
-                callData: configInterface.encodeFunctionData('lpWhitelist', [trader]),
+                callData: configInterface.encodeFunctionData('lpWhitelist', [quoteAddr, trader]),
             });
         }
-        const rawData = await this.context.multiCall3.callStatic.aggregate(calls, overrides ?? {});
-        for (const data of rawData.returnData) {
-            results.push(configInterface.decodeFunctionResult('lpWhitelist', data)[0]);
+        try {
+            const rawData = await this.context.multiCall3.callStatic.aggregate(calls, overrides ?? {});
+            for (const data of rawData.returnData) {
+                results.push(configInterface.decodeFunctionResult('lpWhitelist', data)[0]);
+            }
+            return results;
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (e) {
+            // ignore error since the contract on some network may not have this function
         }
-        return results;
+        return [];
     }
 
     async openLp(quoteAddr?: string, overrides?: CallOverrides): Promise<boolean> {
-        if (
-            (this.context.chainId === CHAIN_ID.BASE ||
-                this.context.chainId === CHAIN_ID.MONADTESTNET ||
-                this.context.chainId === CHAIN_ID.LOCAL) &&
-            quoteAddr
-        ) {
+        if (this.LEGACY_NETWORKS.includes(this.context.chainId)) {
+            return (this.context.perp.contracts.config as LegacyConfig).openLp(overrides ?? {});
+        }
+
+        if (quoteAddr) {
             try {
                 const restricted = await this.context.perp.contracts.config.restrictLp(quoteAddr, overrides ?? {});
                 return !restricted;
@@ -72,6 +79,6 @@ export class ConfigModule implements ConfigInterface {
                 // ignore error since the contract on some network may not have this function
             }
         }
-        return (this.context.perp.contracts.config as LegacyConfig).openLp(overrides ?? {});
+        return false;
     }
 }
